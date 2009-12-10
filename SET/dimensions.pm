bcc8c7b53cfdea971f4906b4ab86749d3d2e66c9
James Avery <avery@diku.dk>
2009-12-10 17:52:02 +1300

    Made dist_x, dist_y, oxide_height and dielectric_constant configurable through config.pm.

--- SET/dimensions.pm
+++ SET/dimensions.pm
@@ -7,7 +7,7 @@ sub max { return $_[0]<=$_[1]? $_[1]: $_[0]; }
 
 %cm = $molecule{$moleculename}{'center of mass'};
 
-$vacuum_width    = $xmax-$xmin+2*$AA; # We want 1Å distance from electrodes to nearest nucleus
+$vacuum_width    = $xmax-$xmin+2*$dist_x; # We want $dist_x distance from electrodes to nearest nucleus
 $vacuum_height   = &min(50*$AA,$vacuum_width*3); # 3 times height of molecule
 				       # (including electrons at both
 				       # ends -- approximated by 2x6
@@ -16,11 +16,8 @@ $vacuum_height   = &min(50*$AA,$vacuum_width*3); # 3 times height of molecule
 				       # thin, tall mesh cells.
 $slice_depth     = &min(50*$AA,$vacuum_width*3); # Ditto.
 $electrode_width = 50*$AA;
-$oxide_height    = 50*$AA;
 
-$dist_y  = 1*$AA-$ymin;		# We want 1Å distance from gate to nearest nucleus
-
-$dielectric_constant = 10.0;
+$translate_y  = $oxide_height+$dist_y-$ymin;		# We want 1Å distance from gate to nearest nucleus
 
 # Left and right electrode get respectively half the source-drain voltage Vsd: V_L = -V/2, V_R = V/2
 ($V_L,$V_R,$V_G) = (-0.5*$Vsd*$eV,0.5*$Vsd*$eV,$Vg*$eV); 
@@ -32,4 +29,4 @@ $dielectric_constant = 10.0;
 ($Oxide_W,$Oxide_H,$Oxide_D) = ($boxW,$oxide_height,$boxD);
 ($Oxide_x,$Oxide_y,$Oxide_z) = ($Oxide_w,$Oxide_h,$Oxide_d);
 
-($Hx,$Hy,$Hz) = ($boxw,$Oxide_H+$dist_y,$boxd);
+($Hx,$Hy,$Hz) = ($boxw,$translate_y,$boxd);
