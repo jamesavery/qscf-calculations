780d870956d7aa5a870929f684f7e3ce41b837f9
James Avery <avery@diku.dk>
2009-12-10 17:48:49 +1300

    *** empty log message ***

---
+++ SET/config.pm
@@ -0,0 +1,6 @@
+$dielectric_constant = 10.0;	# Dielectric constant of gate oxide.
+$dist_x = 1*$AA;		# Distance from either electrode to
+				# nearest nucleus.
+$dist_y = 1*$AA;		# Distance from gate oxide surface to
+				# nearest nucleus.
+
