c88a1ed5e381ecec1b8b859fdbc795bc1b9715cf
anastas7@post.tau.ac.il <avery@diku.dk>
2009-12-12 18:38:54 +0100

    Finished.

--- SET/runs/oxide-experiments.sh
+++ SET/runs/oxide-experiments.sh
@@ -19,34 +19,39 @@ generate_mesh(){
 }
 
 
-for charge in -2.0 -1.0 0.0 1.0 2.0
-  do
+# Exp. 1
+Dy=1
 
-  # Exp. 1
-  Dy=1
-  for Vg in 0 0.01 0.1 0.5 1.0 2.0 4.0; do
-      for H in 10 20 30 40 50 60 70 80 90 100; do
-	  generate_mesh $Dy $H
+for H in 10 20 30 40 50 60 70 80 90 100; do
+    generate_mesh $Dy $H
+    for Vg in 0 1.0 2.0 4.0; do
+	for charge in -1.0 0.0 1.0 
+	  do
 	  ./oxide-input.pl $molecule $charge $Vg $Dy $H $final_dE > ${directory}/Exp1.${charge}:${Vg}:${H}.in
-      done
-  done
-
-  # Exp. 2
-  H=50
-  for Vg in 0 0.01 0.1 0.5 1.0 2.0 4.0; do
-      for Dy in 0.5 1 2 3 5 10; do
-	  generate_mesh $Dy $H
-	  ./oxide-input.pl $molecule $charge $Vg $Dy $H $final_dE > ${directory}/Exp2.${charge}:${Vg}:${Dy}.in
-      done
-  done
-
-  # Exp. 3: As SET-Vzero
-  H=50
-  Dy=1
-  for Vg in -4.0 -3.5 -3.0  -2.5 -2.0  -1.5 -1.0  -0.5 \
-                 0.5 1.0  1.5 2.0 2.5 3.0  3.5 4.0
-    do
-    ./oxide-input.pl $molecule $charge $Vg $Dy $H $final_dE > ${directory}/Exp3.${charge}:${Vg}.in
-  done
+	done
+    done
 done
+
+# Exp. 2
+H=50
+for Dy in 0.5 1 2 3 5 10; do
+    generate_mesh $Dy $H
+    for Vg in 0 1.0 2.0 4.0; do
+	for charge in -1.0 0.0 1.0; do
+	    ./oxide-input.pl $molecule $charge $Vg $Dy $H $final_dE > ${directory}/Exp2.${charge}:${Vg}:${Dy}.in
+	done
+    done
+done
+
+# Exp. 3: As SET-Vzero
+H=50
+Dy=1
+for charge in -2.0 -1.0 0.0 1.0 2.0; do
+    for Vg in -4.0 -3.5 -3.0  -2.5 -2.0  -1.5 -1.0  -0.5 \
+	0.5 1.0  1.5 2.0 2.5 3.0  3.5 4.0
+      do
+      ./oxide-input.pl $molecule $charge $Vg $Dy $H $final_dE > ${directory}/Exp3.${charge}:${Vg}.in
+    done
+done
+
 
