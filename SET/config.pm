a62d8563590e0f0069e270457cde58d8842b9b12
anastas7@post.tau.ac.il <avery@diku.dk>
2009-12-16 12:30:18 +0100

    Added dimensions for vacuum reference calculations.

--- SET/config.pm
+++ SET/config.pm
@@ -1,5 +1,6 @@
 # This file defines parameters for the Single Electron Transistor environment.
 
+# SET-environment parameters
 $dielectric_constant = 10.0;	# Dielectric constant of gate oxide.
 $dist_x = 1*$AA;		# Distance from either electrode to
 				# nearest nucleus.
@@ -7,3 +8,6 @@ $dist_y = 1*$AA;		# Distance from gate oxide surface to
 				# nearest nucleus.
 
 $oxide_height    = 50*$AA;	# Thickness of gate oxide.
+
+# Dimensions for vacuum calculation
+$vacuum_box_size = 200*$AA;	
