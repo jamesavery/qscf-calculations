ca1c8351f8f1523f97f27a6566eb5b1d8634a35c
anastas7@post.tau.ac.il <avery@diku.dk>
2009-12-29 01:08:22 +0100

    Fixed bug.

--- SET/runs/vacuum.sh
+++ SET/runs/vacuum.sh
@@ -10,5 +10,5 @@ mkdir -p $directory
 ln -sf $PWD/bases $PWD/geometries $PWD/opv5parameters.in $PWD/$molecule/
 
 for charge in -2.0 -1.0 0.0 1.0 2.0; do
-    ./vacuum-input.pl $molecule $charge ${directory}/Exp1.${charge}.in 
+    ./vacuum-input.pl $molecule $charge > ${directory}/Exp1.${charge}.in 
 done
