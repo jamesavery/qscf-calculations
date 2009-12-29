e7e28576257378075a9f880e66d568af265a6362
James Avery <avery@diku.dk>
2009-12-29 04:05:13 +0100

    Experiment H-2: Charge and single mirror charge.

---
+++ H-sanity/H-2a.pl
@@ -0,0 +1,95 @@
+#!/usr/bin/perl
+
+# H + charge close to a metal.
+# Method (a): Using a single Dirichlet boundary.
+
+$eV = 0.0367493254;		# 1 eV in Hartrees
+$nm = 18.897259886;		# 1 nm in Bohrs
+
+my ($charge,$dX) = @ARGV;
+
+do 'fe-config.pm';
+
+($boxW,$boxH,$boxD) = (400,400,400);
+($boxw,$boxh,$boxd) = ($boxW/2,$boxH/2,$boxD/2);
+
+($Hx,$Hy,$Hz) = ($dX,$boxh,$boxd);
+
+print << "END"
+
+Hdzp<PaoParam>: (
+        label=H
+        setupfile="H_pz.UPF"
+        {n   l   energyshift   deltarinner  v0  charge splitnorm  polarized} = {
+        1   0      0.005         0.8    20.  0.0     0.0       0
+        1   0      0.005         0.8    20.  0.0     0.15       0
+        2   1      0.005         0.8    20.  0.0     0.0       1
+ }
+)
+
+basissetDZP<BasisSetParam>: (
+        paoparam = [ \$:Hdzp ]
+        energycutoff=2500
+        numberofreciprocalpoints=1024
+        dr=0.01
+)
+
+
+
+H<Molecule>: ( 
+  symmetry = auto
+  unit = angstrom
+  origin = [0 0 0]
+  symmetry_frame = [[1 0 0] [0 0 1] [0 1 0]]
+  { atom_labels atoms geometry } = {
+      H H [ 0 0 0 ]
+  }
+)
+
+
+lattice<Lattice>:(
+	basis=\$:H
+        translate_basis = [$Hx $Hy $Hz]
+	unitcell = [ [ $boxW 0 0 ] [ 0 $boxH 0 ] [ 0 0 $boxD ] ]	
+	unitcell:unit=bohr
+)
+
+calculator<LatticeFEMCalculator>: (	
+    lattice= \$:lattice
+    basisset=\$:basissetDZP
+    boundaryconditions = [ dirichlet dirichlet dirichlet ]
+    meshcutoff:unit=hartree
+    kpoints:monkhorstpack = [1 1 1]
+    electrontemperature = $convergenceparams{electrontemperature}
+    electrontemperature:unit = ev
+
+    charge = $charge
+    initial_refinement=$feparams{'initial_refinement'}
+    final_dE=$feparams{'final_dE'}
+    fe_order = $feparams{'fe_order'}
+    refinement_strategy   = $feparams{'refinement_strategy'}
+    centers_max_diameter  = $feparams{'centers_max_diameter'}
+    centers_near_diameter = $feparams{'centers_near_diameter'}
+    end_refine = $feparams{'end_refine'}
+
+    write_mesh = $feparams{'write_mesh'}
+    write_solution = $feparams{'write_solution'}
+)
+
+qscf<ScfParam>: (
+	calculator=\$:calculator
+	diis:n = 8
+	diis:start = 1
+	diis:dampingfactor = 0.8
+	precondition= false
+        tolerance=$convergenceparams{tolerance}
+	variable=hamiltonian
+        maxiterations=$convergenceparams{maxiterations}
+	verbose=10
+)
+
+
+
+END
+
+};
--- H-sanity/runs/H-2.sh
+++ H-sanity/runs/H-2.sh
@@ -4,12 +4,16 @@ source config.sh;
 
 # TODO: dX=8.0 afsloerer bug i centers_plus_density refinement implementationen.
 
-mkdir -p H2-${final_dE}
-for dX in 6.0 6.25 6.5 6.75 7.0 7.5 8.1 9.0 10.0 13.0 15.0 17.0 20.0 25.0 30.0
+mkdir -p H-2/${final_dE}
+
+# Method (a)
+for dX in 2.0 3.0 4.0 5.0 6.0 6.25 6.5 6.75\
+          7.0 7.5 8.1 9.0 10.0 13.0 15.0 17.0 20.0 25.0 30.0
 do 
-    for charge in -1.0 -0.5 -0.2 0.0 0.2 0.5 0.9
+    for charge in -2.0 -1.5 -1.0 -0.5 -0.2 0.0 0.2 0.5 0.9 0.95
     do
-	./H-2.pl $charge $dX $initial $final_dE > H2-${final_dE}/H.${charge}:${dX}.in
+	./H-2a.pl $charge $dX > H-2/${final_dE}/Ha.${charge}:${dX}.in
     done
 done
 
+# TODO: Method (b): Mesh and fixed region.
