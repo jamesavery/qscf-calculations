af6578a37aff144368b003c69c456562047c42da
James Avery <avery@diku.dk>
2010-01-15 17:41:05 +0100

    Added script to run a list of jobs for a list of molecules.

---
+++ SET/run-all.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source config.sh
+
+molecules=(OPV5-vnl OPV5-GAMESS-B3LYP C60 OPV5-tertbutyl-vnl)
+experiments=(
+    groundzero:SET groundzero2:SET 
+    oxide:Exp1 oxide:Exp2 oxide:Exp3 
+    electrodes:Exp1 electrodes:Exp2 electrodes:Exp2b
+    vacuum:Exp1 Vzero:SET-2 Vzero:SET
+)
+
+for m in ${molecules[*]}; do
+    for exp in ${experiments[*]}; do
+	exp=(`echo $exp | tr ':' ' '`)
+	outdir=results/$m/${final_dE}/
+
+	echo "Running all from $m/${exp[0]}-${final_dE}/ ${exp[1]}.."
+
+	./run-distributed.pl ${exp[1]} $m/${exp[0]}-${final_dE}/
+    done
+done
