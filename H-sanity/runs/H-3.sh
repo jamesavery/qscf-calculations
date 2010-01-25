6c26589d9530e31f4446789c5bdde642f126832b
James Avery <avery@diku.dk>
2010-01-25 17:53:37 +0100

    Added handling of mesh file.

--- H-sanity/runs/H-3.sh
+++ H-sanity/runs/H-3.sh
@@ -3,6 +3,7 @@
 source ./config.sh;
 
 mkdir -p H-3/${final_dE}
+ln -sf ${PWD}/meshes/mesh1.msh H-3/${final_dE}/
 
 for dielectric_constant in 1.0 2.0 5.0 8.0 10.0 20.0 40.0 80.0 200.0 800.0 1600.0 3200.0
 do
@@ -19,7 +20,7 @@ done
 
 for dielectric_constant in 1.0 10.0 10000.0
 do
-    for dX in 6.5 7.0 7.5 8.0 8.5 9.0 9.5 10.0 10.5 11.0 12.0 13.0 15.0 17.0 20.0 25.0 30.0
+    for dX in 6.5 7.0 7.5 8.0 8.5 9.0 9.5 10.0 10.5 11.0 12.0 13.0 15.0 17.0 20.0 26.0 30.0
     do 
 	for charge in -1.0 0.0 0.5
 	do
