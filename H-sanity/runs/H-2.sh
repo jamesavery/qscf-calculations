883011945ea3ef06bddd2a205d35719a2357a7fd
James Avery <avery@diku.dk>
2010-01-25 17:52:37 +0100

    Added H-2b experiment (using fixed volume instead of boundary).

--- H-sanity/runs/H-2.sh
+++ H-sanity/runs/H-2.sh
@@ -5,6 +5,7 @@ source config.sh;
 # TODO: dX=8.0 afsloerer bug i centers_plus_density refinement implementationen.
 
 mkdir -p H-2/${final_dE}
+ln -sf $PWD/meshes/mesh1.msh H-2/${final_dE}
 
 # Method (a)
 for dX in 2.0 3.0 4.0 5.0 6.0 6.25 6.5 6.75\
@@ -13,6 +14,7 @@ do
     for charge in -2.0 -1.5 -1.0 -0.5 -0.2 0.0 0.2 0.5 0.9 0.95
     do
 	./H-2a.pl $charge $dX > H-2/${final_dE}/Ha.${charge}:${dX}.in
+	./H-2b.pl $charge $dX > H-2/${final_dE}/Hb.${charge}:${dX}.in
     done
 done
 
