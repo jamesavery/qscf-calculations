010b754ff2cc0b2306261d6ed5e2ad72bea2ccc7
James Avery <avery@diku.dk>
2010-03-22 13:53:33 +1300

    Kurts benzene-SET setup: V_sd = 0, variable V_g.

---
+++ SET/runs/SET2-Vzero.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+molecule=$1
+
+source config.sh;
+
+directory=$molecule/SET2-Vzero-${final_dE}
+mkdir -p $directory
+
+./SET-mesh2.pl $molecule > $directory/${molecule}-set.geo
+pushd $directory
+gmsh -3 -algo front3d ${molecule}-set.geo
+popd
+ln -sf $PWD/bases $PWD/geometries $PWD/opv5parameters.in $PWD/$molecule/
+
+
+V=0
+for charge in -2.0 -1.0 0.0 1.0 2.0
+  do
+  for ground in 8.0 -6.0 -4.0 -3.0  -2.0 -1.0  0.0  1.0  2.0 3.0 4.0 6.0 8.0
+    do    
+    ./SET-input2.pl $molecule $charge $V $ground $final_dE > ${directory}/SET.${charge}:${ground}:${V}.in
+  done
+done
+
+V=1
+for charge in -2.0 -1.0 0.0 1.0 2.0
+  do
+  for ground in 8.0 6.0 -4.0 -3.0 -2.0 0.0 -1.0 1.0  2.0 3.0  4.0 6.0 8.0 
+    do    
+    ./SET-input2.pl $molecule $charge $V $ground $final_dE > ${directory}/SET-2.${charge}:${ground}:${V}.in
+  done
+done
+
