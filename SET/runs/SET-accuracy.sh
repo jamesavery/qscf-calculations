d8d3f997680cb9f7941f3306bdd037e2a7b045ea
James Avery <avery@diku.dk>
2010-05-25 09:21:29 +0200

    Reduced number of Vg-points.

--- SET/runs/SET-accuracy.sh
+++ SET/runs/SET-accuracy.sh
@@ -15,10 +15,10 @@ ln -sf $PWD/bases $PWD/geometries $PWD/opv5parameters.in $PWD/$molecule/
 
 
 V=2.0
-for dE in 0.01 0.008 0.004 0.001 0.0005 0.00025; do
+for dE in 0.015 0.008 0.004 0.001 0.0005 0.00025; do
 for charge in -2.0 -1.0 0.0 1.0 2.0
   do
-  for ground in -8.0 -5.0 -3.0 -1.0 0.0 1.0  3.0 5.0 8.0
+  for ground in -6.0 -3.0 -1.0 0.0 1.0  3.0 6.0
     do    
     ./SET-input-accuracy.pl $molecule $charge $V $ground $dE > ${directory}/SET-2.${charge}:${ground}:${V}:${dE}.in
   done
