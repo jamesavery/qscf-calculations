635cfbf84027c74509f3008798924a131d37cadc
James Avery <avery@diku.dk>
2010-05-26 06:17:56 +0200

    Fixed wrong call of SET-input.pl ($molecule is no longer used)

--- SET/runs/SET2-Vzero.sh
+++ SET/runs/SET2-Vzero.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-molecule=$1
+molecule="benzene-kurt"
 
 source config.sh;
 
 directory=$molecule/SET2-Vzero-${final_dE}
 mkdir -p $directory
 
-./SET-mesh-kurt.pl $molecule > $directory/${molecule}-set.geo
+./SET-mesh-kurt.pl > $directory/${molecule}-set.geo
 pushd $directory
 gmsh -3 -algo front3d ${molecule}-set.geo
 popd
@@ -19,7 +19,7 @@ for charge in -2.0 -1.0 0.0 1.0 2.0
   do
   for ground in -8.0 -6.0 -4.0 -3.0  -2.0 -1.0  0.0  1.0  2.0 3.0 4.0 6.0 8.0
     do    
-    ./SET-input-kurt.pl $molecule $charge $V $ground $final_dE > ${directory}/SET.${charge}:${ground}:${V}.in
+    ./SET-input-kurt.pl $charge $V $ground > ${directory}/SET.${charge}:${ground}:${V}.in
   done
 done
 
@@ -28,7 +28,7 @@ for charge in -2.0 -1.0 0.0 1.0 2.0
   do
   for ground in -8.0 6.0 -4.0 -3.0 -2.0 0.0 -1.0 1.0  2.0 3.0  4.0 6.0 8.0 
     do    
-    ./SET-input-kurt.pl $molecule $charge $V $ground $final_dE > ${directory}/SET-2.${charge}:${ground}:${V}.in
+    ./SET-input-kurt.pl $charge $V $ground > ${directory}/SET-2.${charge}:${ground}:${V}.in
   done
 done
 
