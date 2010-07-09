c0df0a4eea3cab2a506a3904a74d6ffb8cbf8e09
James Avery <avery@diku.dk>
2010-07-09 23:55:46 +0200

    ./SET-input.pl now figures out final_dE itself; Removed from here.

--- SET/runs/SET-groundzero.sh
+++ SET/runs/SET-groundzero.sh
@@ -19,7 +19,7 @@ for charge in -2.0 -1.0 0.0 1.0 2.0
   for V in -0.5 -0.45 -0.4 -0.35 -0.3 -0.25 -0.2 -0.15 -0.1 -0.05 \
       0.0 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5
     do
-    ./SET-input.pl ${molecule} $charge $V $ground $final_dE > $directory/SET.${charge}:${ground}:${V}.in
+    ./SET-input.pl ${molecule} $charge $V $ground > $directory/SET.${charge}:${ground}:${V}.in
   done
 done
 
