8817c9f91a06ef4ad73d11f9b613fa56942936a9
James Avery <avery@diku.dk>
2010-07-10 00:11:38 +0200

    Changed sampling points.

--- SET/runs/oxide-experiments.sh
+++ SET/runs/oxide-experiments.sh
@@ -24,7 +24,7 @@ Dy=1
 
 for H in 10 20 30 40 50 60 70 80 90 100; do
     generate_mesh $Dy $H
-    for Vg in 0 1.0 2.0 4.0; do
+    for Vg in 0 2.0 4.0; do
 	for charge in -1.0 0.0 1.0 
 	  do
 	  ./oxide-input.pl $molecule $charge $Vg $Dy $H $final_dE > ${directory}/Exp1.${charge}:${Vg}:${H}.in
@@ -36,7 +36,7 @@ done
 H=50
 for Dy in 0.5 1 2 3 5 10; do
     generate_mesh $Dy $H
-    for Vg in 0 1.0 2.0 4.0; do
+    for Vg in 0 2.0 4.0; do
 	for charge in -1.0 0.0 1.0; do
 	    ./oxide-input.pl $molecule $charge $Vg $Dy $H $final_dE > ${directory}/Exp2.${charge}:${Vg}:${Dy}.in
 	done
@@ -47,8 +47,7 @@ done
 H=50
 Dy=1
 for charge in -2.0 -1.0 0.0 1.0 2.0; do
-    for Vg in -4.0 -3.5 -3.0  -2.5 -2.0  -1.5 -1.0  -0.5 \
-	0.5 1.0  1.5 2.0 2.5 3.0  3.5 4.0
+    for Vg in -8.0 -4.0 -3.0  -2.0  -1.0  0 1.0  2.0 3.0  4.0 8.0
       do
       ./oxide-input.pl $molecule $charge $Vg $Dy $H $final_dE > ${directory}/Exp3.${charge}:${Vg}.in
     done
