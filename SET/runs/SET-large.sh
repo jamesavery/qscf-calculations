543a5fa887a9110da15de253541c9a4a40fbf743
James Avery <avery@diku.dk>
2010-07-10 00:05:36 +0200

    Changed sample points.

--- SET/runs/SET-large.sh
+++ SET/runs/SET-large.sh
@@ -14,14 +14,14 @@ popd
 ln -sf $PWD/bases $PWD/geometries $PWD/opv5parameters.in $molecule/
 
 
-for charge in -2.0 -1.0 0.0 1.0 2.0
+for charge in -3.0 -2.0 -1.0 0.0 1.0 2.0 3.0
   do
-  for ground in -6.0 -3.0  0.0  3.0 6.0
+  for ground in -6.0 -2.0 0.0 2.0 6.0
     do    
     for V in -5.0 -4.0 -3.0 -2.0 -1.0 0.0 \
               5.0  4.0  3.0  2.0  1.0 
       do
-      ./SET-input.pl $molecule $charge $V $ground $final_dE > $directory/SET.${charge}:${ground}:${V}.in
+      ./SET-input.pl $molecule $charge $V $ground > $directory/SET.${charge}:${ground}:${V}.in
     done
   done
 done
