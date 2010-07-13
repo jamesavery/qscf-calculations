0ac1f1ab7c3d551c0416363307681c471485632e
James Avery <avery@diku.dk>
2010-07-13 19:06:34 +0200

    Increased charge range to {-3,...,+5}.

--- SET/runs/vacuum.sh
+++ SET/runs/vacuum.sh
@@ -9,6 +9,6 @@ mkdir -p $directory
 
 ln -sf $PWD/bases $PWD/geometries $PWD/opv5parameters.in $PWD/$molecule/
 
-for charge in -2.0 -1.0 0.0 1.0 2.0; do
+for charge in -3.0 -2.0 -1.0 0.0 1.0 2.0 3.0 4.0 5.0; do
     ./vacuum-input.pl $molecule $charge > ${directory}/Exp1.${charge}.in 
 done
