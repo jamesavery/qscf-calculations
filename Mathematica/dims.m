f80c76470fab710ca64ceadf4503ce22211e9589
James Avery <avery@diku.dk>
2010-07-14 00:42:47 +0200

    Mathematica script for calculating molecules.pm-entries.

---
+++ Mathematica/dims.m
@@ -0,0 +1,27 @@
+name = Environment["molecule"];
+basis    = Environment["basis"];
+hosts    = Environment["hosts"];
+		      
+If[basis == $Failed,basis = "OPV5basis"];
+If[hosts == $Failed,hosts = "all"];
+
+SetDirectory[Environment["OPV"]];
+
+Get["./Mathematica/"<>name<>".m"];
+
+coords = #[[3]]&/@molecule;
+centerofmass = 
+ Sum[molecule[[i, 2]]*molecule[[i, 3]], {i, Length[molecule]}]/
+  Total[#[[2]] & /@ molecule];
+  
+{xs, ys, zs} = {#[[1]] & /@ coords, #[[2]] & /@ coords, #[[3]] & /@ coords};
+
+WriteString[Streams[][[1]],"\n'"<>name<>"' => {\n\t"
+    <>"hosts => '"<>hosts<>"',\n\t"
+    <>"basis => '"<>basis<>".in',\n\t"
+    <>"xmin =>", Min[xs], ", xmax => ", Max[xs], ",\n\t"
+    <>"ymin =>", Min[ys], ", ymax => ", Max[ys], ",\n\t"
+    <>"zmin =>", Min[zs], ", zmax => ", Max[zs], ",\n\t"
+    <>"'center of mass' => {'x'=>", centerofmass[[1]], "," <> "'y'=>", centerofmass[[2]], ", 'z'=>", centerofmass[[3]], "}\n}"
+     ];
+
