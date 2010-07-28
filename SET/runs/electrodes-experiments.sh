b230d0cf8d11240a4156949b5f3d64304930d4ed
James Avery <avery@diku.dk>
2010-07-28 22:47:20 +0200

    Changed Expt. 2 to match SET-groundzero2.sh. Added a description of Expt. 1.

--- SET/runs/electrodes-experiments.sh
+++ SET/runs/electrodes-experiments.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# All the experiments in this file use the setup of the molecule
+# sitting between two electrodes at distance Dx Aangstrom from each
+# electrode to nearest nucleus.
+# There is no oxide.
 
 molecule=$1
 
@@ -20,8 +24,10 @@ generate_mesh(){
 
 
 # Exp. 1
+# We vary the distance Dx from the nearest nucleus to each of the electrodes,
+# as well as the voltage drop over the electrodes.
 H=50
-for Dx in 1 2 3 5 10; do
+for Dx in 0.7 1 2 3 5 10; do
     generate_mesh $Dx $H
     for Vsd in 0 0.05 0.10 0.15 0.20 0.25 0.30 0.40 0.50 0.75 1.0
       do
@@ -38,20 +44,8 @@ H=50
 Dx=1
 generate_mesh $Dx $H
 for charge in -2.0 -1.0 0.0 1.0 2.0; do
-    for Vsd in -0.5 -0.45 -0.4 -0.35 -0.3 -0.25 -0.2 -0.15 -0.1 -0.05 \
-	0.0 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5
+    for Vsd in  -2.0 -1.5 -1.0 -0.5 0.0 0.5 1.0 1.5 2.0
       do
       ./electrodes-input.pl $molecule $charge $Vsd $Dx $H $final_dE > ${directory}/Exp2.${charge}:${Vsd}:${Dx}.in
     done
 done
-
-# Exp. 2b: As SET-groundzero2, but without dielectric.
-H=50
-Dx=1
-generate_mesh $Dx $H
-for charge in -2.0 -1.0 0.0 1.0 2.0; do
-   for Vsd in -2.0 -1.5 -1.0 -0.5 0.0 0.5 1.0 1.5 2.0; do
-      ./electrodes-input.pl $molecule $charge $Vsd $Dx $H $final_dE > ${directory}/Exp2b.${charge}:${Vsd}:${Dx}.in
-    done
-done
-
