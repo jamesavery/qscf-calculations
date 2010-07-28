8e3ceecd3bf873908b89fe42477c4647b19d1888
James Avery <avery@diku.dk>
2010-07-28 22:15:26 +0200

    Added descriptions of Experiments 1 and 2; Changed range for Expt. 3 to match current SET-Vzero.sh.

--- SET/runs/oxide-experiments.sh
+++ SET/runs/oxide-experiments.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# All the experiments in this file use the setup of the molecule
+# sitting Dy Aangstrom above an oxide of thickness H. There are no
+# electrodes; For the same experiments in the SET environment, see
+# SET-thickness.sh
 
 molecule=$1
 
@@ -20,11 +24,13 @@ generate_mesh(){
 
 
 # Exp. 1
+# Varies oxide thickness H while keeping the distance Dy fixed at 0.1nm.
+# We want to obtain U, Delta, E_C and possibly \alpha as a function of H
 Dy=1
 
 for H in 10 20 30 40 50 60 70 80 90 100; do
     generate_mesh $Dy $H
-    for Vg in 0 2.0 4.0; do
+    for Vg in 0.0 2.0 4.0; do
 	for charge in -1.0 0.0 1.0 
 	  do
 	  ./oxide-input.pl $molecule $charge $Vg $Dy $H $final_dE > ${directory}/Exp1.${charge}:${Vg}:${H}.in
@@ -33,6 +39,9 @@ for H in 10 20 30 40 50 60 70 80 90 100; do
 done
 
 # Exp. 2
+# We keep the oxide thickness fixed at 5nm and vary the distance Dy from the
+# oxide to neares nucleus of the molecule.
+# We want to obtain U, Delta, E_C and possibly \alpha as a function of H
 H=50
 for Dy in 0.5 1 2 3 5 10; do
     generate_mesh $Dy $H
@@ -46,11 +55,10 @@ done
 # Exp. 3: As SET-Vzero, but without electrodes.
 H=50
 Dy=1
-for charge in -2.0 -1.0 0.0 1.0 2.0; do
-    for Vg in -8.0 -4.0 -3.0  -2.0  -1.0  0 1.0  2.0 3.0  4.0 8.0
-      do
-      ./oxide-input.pl $molecule $charge $Vg $Dy $H $final_dE > ${directory}/Exp3.${charge}:${Vg}.in
-    done
+for charge in -3.0 -2.0 -1.0 0.0 1.0 2.0 3.0 4.0 5.0
+ for Vg in -8.0 -4.0 -1.0  0.0 1.0 4.0 8.0
+ do
+     ./oxide-input.pl $molecule $charge $Vg $Dy $H $final_dE > ${directory}/Exp3.${charge}:${Vg}.in
+ done
 done
-
 
