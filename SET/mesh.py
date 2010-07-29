554eb9bac0dcae75f15216d86585716ff99cee08
James Avery <avery@diku.dk>
2010-07-30 01:49:31 +0200

    GMSH generator works!

--- SET/mesh.py
+++ SET/mesh.py
@@ -9,17 +9,24 @@ class Volume:
         self.ll = ll;
         self.dims = array(dims);
         self.name = name;
-                       
+
+    def pointInside(self,x):
+        return ((x >= self.ll).all() and (x <= self.ll+self.dims).all());
+
+    def containedIn(self,V):
+        return ((V.ll >= self.ll).all() and (V.ll+V.dims <= self.ll+self.dims).all());            
+        
+    def __repr__(self):
+        return repr((self.name,self.ll,self.dims));
 
 class Space:
 
     def __init__(self,dims):
         self.dims  = array(dims);
-        self.volumes = [Volume([0,0,0],dims)];
+        self.volumes = [Volume([0,0,0],dims,"World")];
         self.xs = [];
         self.ys = [];
         self.zs = [];
-        
 
     def addVolume(self,B):
         self.volumes.append(B);
@@ -43,5 +50,20 @@ class Space:
 
         return zip(lowerlefts,lengths);
 
+    def identifyBox(self,B):
+        return [V.name for V in self.volumes[1:] if V.containedIn(B)];
 
+    def gmshBoxes(self):
+        box_volumes  = [Volume(self.lowerlefts[i],self.lengths[i],i) for i in range(len(self.lengths))];
+        volume_names = [self.identifyBox(V) for V in box_volumes];
 
+        script = "";
+        for i in range(len(box_volumes)):
+            V = box_volumes[i];
+            script += "// Box %d, contained in %s\n" % (i+1 ,volume_names[i]);
+            script += "lowerleft[] = {%g,%g,%g};\n" % (V.ll[0],V.ll[1],V.ll[2]);
+            script += "w = %g;\nh = %g;\nd = %g;\nCall BOX;\n\n" % (V.dims[0],V.dims[1],V.dims[2]);
+
+        return script;
+
+    
