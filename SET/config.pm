45c1e9c0010b3c207032873ac61442df08f6e991
James Avery <avery@diku.dk>
2010-07-30 03:06:59 +0200

    Changed all input lengths to AA.

--- SET/config.pm
+++ SET/config.pm
@@ -2,12 +2,12 @@
 
 # SET-environment parameters
 $dielectric_constant = 10.0;	# Dielectric constant of gate oxide.
-$dist_x = 1*$AA;		# Distance from either electrode to
-				# nearest nucleus.
-$dist_y = 1*$AA;		# Distance from gate oxide surface to
-				# nearest nucleus.
+$dist_x = 1;		# Distance in AA from either electrode to
+			# nearest nucleus.
+$dist_y = 1;		# Distance in AA from gate oxide surface to
+			# nearest nucleus.
 
-$oxide_height    = 50*$AA;	# Thickness of gate oxide.
+$oxide_height    = 50;	# Thickness of gate oxide in AA.
 
-# Dimensions for vacuum calculation
-$vacuum_box_size = 2000*$AA;	
+# Dimensions in AA for vacuum calculation
+$vacuum_box_size = 2000;	
