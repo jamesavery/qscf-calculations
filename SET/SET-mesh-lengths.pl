c0044d3c58e6405682bc39230e826a41afb6f766
James Avery <avery@diku.dk>
2010-07-30 03:19:33 +0200

    Forgot to set oxideH.

--- SET/SET-mesh-lengths.pl
+++ SET/SET-mesh-lengths.pl
@@ -7,5 +7,7 @@ do 'molecules.pm';
 do 'config.pm';
 do 'dimensions.pm';		# Calculate box{W,H,D}, but in Bohrs 
 
+$oxideH = $H;
+
 system("./SET-mesh-allvars.py $boxW $boxH $boxD $vacuumW $vacuumH $oxideH $electrodeH");
 
