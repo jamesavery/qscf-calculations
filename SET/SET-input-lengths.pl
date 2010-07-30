84254d6855c86f9095b2c287813be2ba364efb52
James Avery <avery@diku.dk>
2010-07-30 02:54:42 +0200

    dimensions.pm gives lengths in Aangstrom.

--- SET/SET-input-lengths.pl
+++ SET/SET-input-lengths.pl
@@ -4,17 +4,17 @@ do 'molecules.pm';
 
 ($moleculename, $charge,$Vg,$Vsd,$Dx,$Dy,$H) = @ARGV;
 
-$meshsuffix = "set-$Dx-$Dy-$H";
-$outputdir  = "set-$Dx-$Dy-$H"; # I'm mostly interested in the different meshes.
-                                # If I need solutions as well, charge and V{sd,g} 
-                                # need to be added to the path.
+$meshsuffix = "set-${Dx}-${Dy}-${H}";
+$outputdir  = "set-${Dx}-${Dy}-${H}"; # I'm mostly interested in the different meshes.
+                                      # If I need solutions as well, charge and V{sd,g} 
+                                      # need to be added to the path.
 
 do 'config.pm';
 do 'dimensions.pm';		# Calculate box{W,H,D}, but in Bohrs 
 do 'fe-config.pm';
 
 # Input to x-allvars.sh must be in Aangstrom, not in Bohrs.
-($boxW,$boxH,$boxD) = ($boxW/$AA,$boxH/$AA,$boxD/$AA);
+($boxW,$boxH,$boxD) = ($boxW,$boxH,$boxD);
 
 
 system("./SET-input-allvars.pl $moleculename $charge $Vg $Vsd"
