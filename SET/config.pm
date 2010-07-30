a701a4428d0db8ebb8c20d8e0917ce851718213e
James Avery <avery@diku.dk>
2010-07-30 03:19:02 +0200

    Homogenized variable names.

--- SET/config.pm
+++ SET/config.pm
@@ -7,7 +7,7 @@ $dist_x = 1;		# Distance in AA from either electrode to
 $dist_y = 1;		# Distance in AA from gate oxide surface to
 			# nearest nucleus.
 
-$oxide_height    = 50;	# Thickness of gate oxide in AA.
+$oxideH    = 50;	# Thickness of gate oxide in AA.
 
 # Dimensions in AA for vacuum calculation
 $vacuum_box_size = 2000;	
