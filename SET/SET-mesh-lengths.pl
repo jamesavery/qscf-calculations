45d83fc5aebd9704badbf399e0d8bb7f393e00f8
James Avery <avery@diku.dk>
2010-07-30 02:08:23 +0200

    Now uses python mesh generator.

--- SET/SET-mesh-lengths.pl
+++ SET/SET-mesh-lengths.pl
@@ -9,5 +9,5 @@ do 'dimensions.pm';		# Calculate box{W,H,D}, but in Bohrs
 
 ($boxW,$boxH,$boxD) = ($boxW/$AA,$boxH/$AA,$boxD/$AA);
 
-system("./SET-mesh-allvars.pl $moleculename $Dx $Dy $H $boxW $boxH $boxD");
+system("./SET-mesh-allvars.py $boxW $boxH $boxD $vacuumW $vacuumH $oxideH $electrodeH");
 
