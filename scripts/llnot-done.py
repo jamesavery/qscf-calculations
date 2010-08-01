f3f70e668d6f3293222be49b4bbc63c48d78a256
James Avery <avery@diku.dk>
2010-08-01 21:01:52 +0200

    Don't sort output.

--- scripts/llnot-done.py
+++ scripts/llnot-done.py
@@ -12,7 +12,7 @@ ins    = [f for f in files if f.startswith("qscf") and f.endswith(".out")];
 inbase  = frozenset([f.split('.')[2] for f in ins]);
 outbase = frozenset([f.split('.')[2] for f in outs]);
 
-not_done = sort(list(inbase-outbase));
+not_done = list(inbase-outbase);
 
 for j in not_done:
     print j;
