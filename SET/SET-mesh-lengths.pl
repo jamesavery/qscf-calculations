26212a1fd4ee64c776312fd550daa255ea32d401
James Avery <avery@diku.dk>
2010-08-03 01:06:50 +0200

    Kurwa! dist_x and dist_y were *not* being set.

--- SET/SET-mesh-lengths.pl
+++ SET/SET-mesh-lengths.pl
@@ -6,6 +6,8 @@ do 'molecules.pm';
 
 do 'config.pm';
 $oxideH = $H;
+$dist_x = $Dx;
+$dist_y = $Dy;
 do 'dimensions.pm';		# Calculate box{W,H,D}, but in Bohrs 
 
 system("./SET-mesh-allvars.py $boxW $boxH $boxD $vacuumW $vacuumH $oxideH $electrodeH");
