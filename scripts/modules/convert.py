6be9c1fd49e0dd0cbbce2d13ec99f4552e895c70
James Avery <avery@diku.dk>
2010-08-03 21:03:41 +0200

    Added option to replace singleton lists with their single element (used in input to result.m).

--- scripts/modules/convert.py
+++ scripts/modules/convert.py
@@ -1,10 +1,13 @@
-def mathematica_repr(x):
+def mathematica_repr(x,collapse=False):
     if type(x) == list or type(x) == tuple:
-        return mathematica_list(x);
+        return mathematica_list(x,collapse);
     elif type(x) == float:
         return ("%f" % x);
     else:
         return repr(x);
 
-def mathematica_list(L):
-    return "{"+(",".join([mathematica_repr(x) for x in L]))+"}";
+def mathematica_list(L,collapse=False):
+    if(collapse and len(L) == 1):
+        return mathematica_repr(L[0],collapse);
+    else:
+        return "{"+(",".join([mathematica_repr(x,collapse) for x in L]))+"}";
