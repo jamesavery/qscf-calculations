44b36ef8f438c0a400d6c2fef3aaa6f4fb510b62
James Avery <avery@diku.dk>
2010-08-03 21:15:34 +0200

    Now handles difference to Mathematica's floating point representation (XeN ~> X*^N) in a sane way.

--- scripts/modules/convert.py
+++ scripts/modules/convert.py
@@ -1,8 +1,18 @@
+from math import log, fabs, floor;
+
 def mathematica_repr(x,collapse=False):
     if type(x) == list or type(x) == tuple:
         return mathematica_list(x,collapse);
     elif type(x) == float:
-        return ("%f" % x);
+        if(x==int(x)):
+            return repr(x);
+        else:
+            e = floor(log(fabs(x))/log(10));
+            if e <= -5 or e > 6:
+                m = x/(10**e);
+                return "%f*^%d" % (m,e);
+            else:
+                return "%f" % x;
     else:
         return repr(x);
 
