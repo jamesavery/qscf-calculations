5967738a6310527cd99ff8b26120cf43ef80a5f3
James Avery <avery@diku.dk>
2010-08-03 21:17:41 +0200

    Script to collect total energies for a single experiment.

---
+++ scripts/collect-one.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use File::Basename;
+use Cwd 'abs_path';
+
+$root   = $ENV{'OPV'};
+my $exp2 = $ARGV[0];
+my $path = abs_path($ARGV[1]);
+
+if($path =~ /([^\/]+)\/([^\/]+)-([0-9\.]+)\/([^\/]+)$/){
+    ($m,$exp1,$dE,$jobid) = ($1,$2,$3);
+} else {
+   print "Source dir $path doesn't match correct pattern.\n";
+   exit -1;
+}
+
+
+$outdir ="${root}/results/$m/$dE";
+$outfile="${outdir}/${exp1}-${exp2}.m";
+
+`mkdir -p ${outdir}`;
+print "Generating ${outfile} from $m/${exp1}-$dE/${exp2}.*.out\n";
+
+`energies-math.pl $path ${exp2} > ${outfile}`;
+
