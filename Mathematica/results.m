fa31b151dd9e09b9c39f5c8bc43e002fdeb7ac10
James Avery <avery@diku.dk>
2010-08-03 23:34:22 +0200

    +Factored out reshape[]-function +Added HOMO/LUMO-handling +Added handling for the case when Length[order] == 1.

--- Mathematica/results.m
+++ Mathematica/results.m
@@ -8,7 +8,6 @@ getData[molecule_,dE_,exp1_,exp2_] := Module[{info,tidx,tE,dims,order,data,param
       info["order"] = {"q"}; 
       info["data"] = Sort[propertylist];
       info["q"] = #[[1]]&/@ info["data"];
-      info["energy"] = #[[2]]&/@ info["data"];
       ,
   {exp1,exp2}=={"large","SET"} || 
   {exp1,exp2}=={"Vzero","SET"} || 
@@ -19,7 +18,6 @@ getData[molecule_,dE_,exp1_,exp2_] := Module[{info,tidx,tE,dims,order,data,param
       info["q"]      = #[[1,1]]&/@ info["data"];
       info["Vg"]     = #[[1,2]]&/@ info["data"];
       info["Vsd"]    = #[[1,3]]&/@ info["data"];
-      info["energy"] = #[[2]]&/@ info["data"];
       ,
   {exp1,exp2}=={"SET-lengths","Exp1"},
       info["order"] = {"q","Vg","oxideH"}; 
@@ -27,7 +25,6 @@ getData[molecule_,dE_,exp1_,exp2_] := Module[{info,tidx,tE,dims,order,data,param
       info["q"]      = #[[1,1]]&/@ info["data"];
       info["Vg"]     = #[[1,2]]&/@ info["data"];
       info["oxideH"] = #[[1,3]]&/@ info["data"];
-      info["energy"] = #[[2]]&/@ info["data"];
       ,
   {exp1,exp2}=={"SET-lengths","Exp2"},
       info["order"] = {"q","Vg","dist_y"}; 
@@ -35,51 +32,68 @@ getData[molecule_,dE_,exp1_,exp2_] := Module[{info,tidx,tE,dims,order,data,param
       info["q"]      = #[[1,1]]&/@ info["data"];
       info["Vg"]     = #[[1,2]]&/@ info["data"];
       info["dist_y"] = #[[1,3]]&/@ info["data"];
-      info["energy"] = #[[2]]&/@ info["data"];
       ,
   {exp1,exp2}=={"SET-lengths","Exp3"},
-      info["order"] = {"q","Vg","dist_x"}; 
+      info["order"] = {"q","Vg","Vsd","dist_x"}; 
       info["data"] = Sort[propertylist];
       info["q"]      = #[[1,1]]&/@ info["data"];
       info["Vg"]     = #[[1,2]]&/@ info["data"];
-      info["dist_x"] = #[[1,3]]&/@ info["data"];
-      info["energy"] = #[[2]]&/@ info["data"];
+      info["Vsd"]    = #[[1,3]]&/@ info["data"];
+      info["dist_x"] = #[[1,4]]&/@ info["data"];
       ,
   {exp1,exp2}=={"SET-lengths","Exp4"},
       info["order"] = {"q","Vsd","dist_x"}; 
       info["data"] = Sort[propertylist];
       info["q"]      = #[[1,1]]&/@ info["data"];
-      info["Vg"]     = #[[1,2]]&/@ info["data"];
+      info["Vsd"]     = #[[1,2]]&/@ info["data"];
       info["dist_x"] = #[[1,3]]&/@ info["data"];
-      info["energy"] = #[[2]]&/@ info["data"];
   ];
+
+  info["energy"] = #[[2]]&/@ info["data"];
+  info["HLN"]    = #[[3]]&/@ info["data"];
 					     
   If[Length[info["order"]]>1,
       order  = info["order"];
       data   = info["data"];
-      energy = #[[2]]& /@ data;
       params = Union[info[#]]& /@ order;
       dims   = Length /@ params;
-      muls   = Table[Product[dims[[i]],{i,j,Length[dims]}],{j,2,Length[dims]}];
       
-      Switch[Length[order],
-        2,
-          tidx = Outer[{#1,#2}&,params[[1]],params[[2]]];
-          tE   = Table[energy[[(i-1)*muls[[1]]+j]],{i,Length[params[[1]]]},{j,Length[params[[2]]]}];
-        3,
-          tidx = Outer[{#1,#2,#3}&,params[[1]],params[[2]],params[[3]]];
-          tE   = Table[energy[[(i-1)*muls[[1]]+(j-1)*muls[[2]]+k]],
-                {i,Length[params[[1]]]},{j,Length[params[[2]]]},{k,Length[params[[3]]]}];
-        4,
-          tidx = Outer[{#1,#2,#3,#4}&,params[[1]],params[[2]],params[[3]],params[[4]]];
-          tE   = Table[energy[[(i-1)*muls[[1]]+(j-1)*muls[[2]]+(k-1)*muls[[3]]+l]],
-                {i,Length[params[[1]]]},{j,Length[params[[2]]]},
-                {k,Length[params[[3]]]},{l,Length[params[[4]]]}];
-      ];
       info["params"]      = params;
       info["dims"]        = dims;
-      info["indextable"]  = tidx;
-      info["energytable"] = tE;
+      info["indextable"]  = reshape[#[[1]]&/@data,dims];
+      info["energytable"] = reshape[#[[2]]&/@data,dims];
+      info["HOMOtable"]   = reshape[#[[3,1]]&/@data,dims];
+      info["LUMOtable"]   = reshape[#[[3,2]]&/@data,dims];
+      info["Ntable"]      = reshape[#[[3,3]]&/@data,dims];
+      ,
+      order  = info["order"];
+      data   = info["data"];
+
+      info["params"]      = Union[info[#]]& /@ order;
+      info["dims"]        = {1};
+      info["indextable"]  = #[[1]]&/@data,dims;
+      info["energytable"] = #[[2]]&/@data,dims;
+      info["HOMOtable"]   = #[[3,1]]&/@data,dims;
+      info["LUMOtable"]   = #[[3,2]]&/@data,dims;
+      info["Ntable"]      = #[[3,3]]&/@data,dims;
   ];
   Return[info];
+];
+
+reshape[t_,dims_] := Module[{n = Length[dims],tE},
+  muls   = Table[Product[dims[[i]],{i,j,Length[dims]}],{j,2,Length[dims]}];
+
+  Switch[n,
+        2,
+          tE = Table[t[[(i-1)*muls[[1]]+j]],{i,dims[[1]]},{j,dims[[2]]}];
+        3,
+          tE   = Table[t[[(i-1)*muls[[1]]+(j-1)*muls[[2]]+k]],
+                {i,dims[[1]]},{j,dims[[2]]},{k,dims[[3]]}];
+        4,
+          tE   = Table[t[[(i-1)*muls[[1]]+(j-1)*muls[[2]]+(k-1)*muls[[3]]+l]],
+                {i,dims[[1]]},{j,dims[[2]]},
+                {k,dims[[3]]},{l,dims[[4]]}];
+      ];
+
+  Return[tE];
 ];
