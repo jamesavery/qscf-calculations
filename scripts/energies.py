b939e5186b49709bdeee3fd3ce3fc1e9317bb2f3
James Avery <avery@diku.dk>
2010-08-03 20:59:50 +0200

    Fixed a number of issues. Added Mathematica output.

--- scripts/energies.py
+++ scripts/energies.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 from __future__ import with_statement;
 from modules.experiments import *;
-from os import environ, path;
-from sys import argv;
-from string import split, join, strip;
+from modules.convert import *;
 from modules.scanf import sscanf;
+from os import environ, path;
+from sys import argv,stdout,stderr;
+from string import split, join, strip;
+import re;
 
 files = [path.abspath(f) for f in argv[1:]];
 root = environ['OPV'];
+
+def unique(l):
+    s = list(frozenset(l));
+    if(len(s)>1):
+        raise ValueError("Multiple unique values in list.");
+    return s[0];
 
 def parsepath(f):
     if(path.basename(argv[0]) == "energies.py"):
@@ -22,8 +30,9 @@ def parsepath(f):
     return (molecule, exp1, exp2, final_dE);
 
 def fileparameters(f):
-    base = path.basename(f);
-    return [s for l in split(base,'.') for s in split(l,':') ][1:-1];
+    base = path.splitext(path.basename(f))[0];
+    m = re.search("([^\.]+)\.(.+)",base);
+    return [float(p) for p in split(m.group(2),':')];
                  
 def totalenergy(f):
     f = path.splitext(f)[0]+".out";
@@ -32,7 +41,7 @@ def totalenergy(f):
         energy = sscanf(eline,"Total potential energy  = %f eV")[0];
         return energy;
 
-def homolumuenergy(f):
+def homolumoenergy(f):
     f = path.splitext(f)[0]+".err";
     with open(f,'r') as file:
         lines  = file.readlines();
@@ -41,32 +50,42 @@ def homolumuenergy(f):
         tup = lines[eline-1].split('= ')[1];
         (T,degeneracy,numberElectrons) = sscanf(tup,"(%f,%d,%d)");
 
-        return (energies[numberElectrons/degeneracy],
-                energies[numberElectrons/degeneracy+1],
+        return (float(energies[(numberElectrons-1)/degeneracy]),
+                float(energies[(numberElectrons-1)/degeneracy+1]),
                 numberElectrons);
 
-
-def getinfo(files):
+def getinfo(f):
     finfo = parsepath(f);
     expt  = experiments[finfo[1]][finfo[2]];
     params = zip(expt['order'],fileparameters(f));
     energy = totalenergy(f);
-    (EH,EL,Nel) = homolumuenergy(f);
+    (EH,EL,Nel) = homolumoenergy(f);
 
     return {
         'energy':energy,
-#        'homo':EH,
-#        'lumo':EL,
+        'homo':EH,
+        'lumo':EL,
         'params':params
         };
 
-    
 
-expts = list(set([parsepath(f) for f in files]));
-info = [getinfo(f) for f in files];
+def mathematica_output(file,finfo,file_list):
+    expt  = experiments[finfo[1]][finfo[2]];
+    params   = [fileparameters(f) for f in file_list];
+    energies = [totalenergy(f)    for f in file_list];
+    HLN      = [homolumoenergy(f) for f in file_list];
 
-print expts;
-for I in info:
-    print I;
+    print >> file, "order = %s;\n" % mathematica_list(expt['order']);
 
-print [I['energy'] for I in info];
+    propertylist = [(params[i],energies[i],HLN[i]) for i in range(len(file_list))];
+    print >> file, "propertylist = %s;\n" % mathematica_list(propertylist);
+
+
+ 
+try:
+    finfo = unique([parsepath(f) for f in files]);
+except ValueError:
+    print >> stderr, "Only one experiment at a time for now.";
+    exit(-1);
+
+mathematica_output(stdout,finfo,files);
