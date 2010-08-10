4a10f464db771af7a3ba295426797c0339955889
James Avery <avery@diku.dk>
2010-08-10 21:30:45 +0200

    QM vacuum .m files should return a result.

--- scripts/firefly-collect.py
+++ scripts/firefly-collect.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from modules.convert import *;
-from modules.qm_collect import *;
+from modules.collect import *;
 
 [molecule] = argv[1:];
 
 energies = collect_molecule("Firefly",molecule);
 
-print "results = %s;\n" % mathematica_repr(energies);
+print mathematica_repr(energies);
