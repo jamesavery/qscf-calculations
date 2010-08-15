c35c78071f41e47ce39c158de231b37c6f82dd74
James Avery <avery@diku.dk>
2010-08-15 22:36:55 +0200

    Include convergence status in all results.

---
+++ SET/results/benzene-kurt/0.0007/vacuum-Exp1.m
@@ -0,0 +1,8 @@
+experiment = {"benzene-kurt","vacuum","Exp1","0.0007"};
+
+parameterOrder = {"charge"};
+
+valueOrder = {"Total energy",{"HOMO energy","LUMO energy","Number of electrons"},"Converged"};
+
+propertylist = {{0.0,-1039.135000,{0,0,0},True},{-1.0,-1036.701100,{0,0,0},True},{1.0,-1029.981000,{0,0,0},True},{-2.0,-1028.244600,{0,0,0},True},{2.0,-1014.343900,{0,0,0},True},{-3.0,-1013.970400,{0,0,0},True},{3.0,-991.914500,{0,0,0},True},{4.0,-962.639900,{0,0,0},True},{5.0,-925.807000,{0,0,0},True}};
+
