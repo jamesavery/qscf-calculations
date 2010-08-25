3c015f31f36a6663e9ba57f4e873c8e71e8b9b58
James Avery <avery@diku.dk>
2010-08-25 02:50:22 +0200

    Made Delta[] work despite missing points. Assumes DE^q is piecewise linear in q, and whenever the gap between two data points is greater than one, that DE^q is not discontinuous in this point.

--- Mathematica/vacuum-compare.m
+++ Mathematica/vacuum-compare.m
@@ -1,7 +1,24 @@
 Get[Environment["OPV"]<>"/../Mathematica/results.m"];
 Needs["ComputerArithmetic`"];
 
-Delta[t_] := Table[{t[[i+1,1]],t[[i+1,2]] - t[[i,2]]}, {i, Length[t] - 1}];
+Delta[t_] := Module[{q1,e1,q0,e0,D},
+  D = Table[0,{Length[t]-1}];
+  Do[
+    {q0,e0} = t[[i]];
+    {q1,e1} = t[[i+1]];
+    D[[i]] = If[q1-q0==1,{q0,e1-e0},
+                {q1-1,delta[q1-q0,e1,e0,D[[i-1,2]]]}
+             ],
+    {i,Length[D]}
+  ];
+  Return[D];
+];
+
+delta[n_,En_,E1_,DE0_] := Module[{a},
+  a = 2/(n+1)*((En-E1)/n-DE0);
+  Return[DE0+a*n];
+]
+
 Hartrees = 27.21138386;
 
 vacuumData[molecule_,dE_]:= Module[{M,cqe},
@@ -50,35 +67,47 @@ vacuumCheck[program_,molecule_] := Module[{olist,M,Ms,functional,basis},
 
 
 
-checkPlot[tecs_,list_,plotlabel_] := Module[{fits0,g1,g2},
+Get["../Mathematica/makelegend.m"];
 
-  fits0 = Fit[fromzero[#],{1,q,q^2},q] & /@ tecs;
+checkPlotB[tecs_,list_,plotlabel_] := Module[{funs0,legend,g1,g2,llength,flimits,ftable},
+  funs0 = Interpolation[fromzero[#],InterpolationOrder->2] & /@ tecs;
+  flimits = #[[1,1]]&/@funs0;
 
-  g1 = ListPlot[Delta /@ tecs, Joined -> True, 
-  PlotStyle -> PointSize -> 0.01, PlotMarkers -> Automatic, 
-  ImageSize -> 500, AxesLabel -> {q, "\!\(\*
-  StyleBox[SuperscriptBox[\"E\", 
-  RowBox[{\"q\", \"+\", \"1\"}]],\nFontSlant->\"Italic\"]\)\!\(\*
-  StyleBox[\"-\",\nFontSlant->\"Italic\"]\)\!\(\*
-  StyleBox[SuperscriptBox[\"E\", \"q\"],\nFontSlant->\"Italic\"]\) in \
-  eV"}, PlotLabel -> plotlabel, 
-  PlotLegend -> FileNameJoin /@ list, LegendSize -> {.7, .4}, 
-  LegendPosition -> {-1, .2}];
+  llength = Max[StringLength /@ FileNameJoin /@ list];
+  legend = makeLegend[FileNameJoin/@list,
+                      {LegendSize->{.2+llength*.05,.08*Length[list]},
+                       LegendPosition->{-1.2,0}}];
+  ftable = Table[{q,funs0[[i]][q]},{i,Length[funs0]},{q,flimits[[i,1]],flimits[[i,2]],.1}];
 
-  g2 = Show[
-  Plot[fits0, {q, -3, 3}],
-  ListPlot[fromzero /@ tecs, Joined -> False, 
-  PlotStyle -> PointSize -> 0.01, PlotMarkers -> Automatic],
-  ImageSize -> 500, AxesLabel -> {q, "\!\(\*
-  StyleBox[SuperscriptBox[\"E\", \"q\"],\n\
-  FontSlant->\"Italic\"]\)\!\(\*
-  StyleBox[\"-\",\nFontSlant->\"Italic\"]\)\!\(\*
-  StyleBox[SuperscriptBox[\"E\", \"0\"],\nFontSlant->\"Italic\"]\) in \
-  eV"}, PlotLabel -> plotlabel
-  ];
-
-  Return[{g1,g2}];
+  ShowLegend[
+    Show[
+        ListPlot[ftable, Joined->True,
+        PlotStyle->Thick, PlotLabel->plotlabel,
+        AxesLabel->{q,"\!\(\*SuperscriptBox[\"E\",q]-\*SuperscriptBox[\"E\",0]\)"}],
+        ListPlot[fromzero/@tecs,PlotStyle->PointSize[.018]]
+    ],
+    legend
+  ]
 ];
+
+checkPlotC[tecs_,list_,plotlabel_] := Module[{fits0,legend,g1,g2},
+  llength = Max[StringLength /@ FileNameJoin /@ list];
+  legend = makeLegend[FileNameJoin/@list,
+                      {LegendSize->{.2+llength*.05,.08*Length[list]},
+                       LegendPosition->{-1.2,0}}];
+
+  ShowLegend[
+    Show[
+        ListPlot[Delta/@tecs,Joined->True,
+        PlotStyle->Thick,PlotLabel->plotlabel,
+        AxesLabel->{q,"\!\(\*SuperscriptBox[\"E\",\"q+1\"]-\*SuperscriptBox[\"E\",\"q\"]\)"}],
+        ListPlot[Delta/@tecs,PlotStyle->PointSize[.018]]
+    ],
+    legend
+  ]
+];
+
+checkPlot[tecs_,list_,plotlabel_] := {checkPlotB[tecs,list,plotlabel],checkPlotC[tecs,list,plotlabel]}
 
 compareNumbers[qes_] := Module[{allqs,square},
   allqs = Union @@ (First /@ #& /@ qes);
