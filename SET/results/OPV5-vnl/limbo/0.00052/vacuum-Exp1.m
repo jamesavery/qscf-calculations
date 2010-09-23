b5591fc193510801c9f9cd242fcebdbea3b95ab2
James Avery <avery@diku.dk>
2010-09-23 19:45:36 +0200

    Finally finished Gaussian '03 calculation for q=+3 (23 hours!).

---
+++ SET/results/OPV5-vnl/limbo/0.00052/vacuum-Exp1.m
@@ -0,0 +1,8 @@
+experiment = {"OPV5-vnl","vacuum","Exp1","0.00052"};
+
+parameterOrder = {"charge"};
+
+valueOrder = {"Total energy",{"HOMO energy","LUMO energy","Number of electrons"},"Converged"};
+
+propertylist = {{0.0,-7172.934100,{0,0,0},True},{-1.0,-7174.071300,{0,0,0},True},{1.0,-7166.137000,{0,0,0},True},{-2.0,-7169.783900,{0,0,0},True},{2.0,-7158.532500,{0,0,0},True}};
+
