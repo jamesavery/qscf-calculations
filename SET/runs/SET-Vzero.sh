ca76c81ec1b49fa1fae483bb2f761eaea47f2fa4
James Avery <avery@diku.dk>
2010-10-07 18:08:49 +0200

    q=-5,...,+5 refine mesh once before start.

--- SET/runs/SET-Vzero.sh
+++ SET/runs/SET-Vzero.sh
@@ -10,12 +10,13 @@ mkdir -p $directory
 ./SET-mesh.pl $molecule > $directory/${molecule}-set.geo
 pushd $directory
 gmsh -3 -algo front3d ${molecule}-set.geo
+gmsh -refine ${molecule}-set.msh
 popd
 ln -sf $PWD/bases $PWD/geometries $PWD/opv5parameters.in $PWD/$molecule/
 
 
 Vsd=0
-for charge in -3.0 -2.0 -1.0 0.0 1.0 2.0 3.0 4.0 5.0
+for charge in -5.0 -4.0 -3.0 -2.0 -1.0 0.0 1.0 2.0 3.0 4.0 5.0
   do
   for Vg in -4.0 0.0 1.0 4.0
     do    
@@ -24,7 +25,7 @@ for charge in -3.0 -2.0 -1.0 0.0 1.0 2.0 3.0 4.0 5.0
 done
 
 Vsd=1
-for charge in -3.0 -2.0 -1.0 0.0 1.0 2.0 3.0 4.0 5.0
+for charge in -5.0 -4.0 -3.0 -2.0 -1.0 0.0 1.0 2.0 3.0 4.0 5.0
   do
   for Vg in -4.0 0.0 1.0 4.0
     do    
