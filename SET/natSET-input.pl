ed37d94abcf42a99db425d3afc612ff77f84e1ee
James Avery <avery@diku.dk>
2010-10-07 19:59:39 +0200

    Nature SET.

---
+++ SET/natSET-input.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+do 'molecules.pm';
+
+($moleculename, $charge,$Vg,$Vsd,$feconfig) = @ARGV;
+
+$meshsuffix = "natSET";
+$outputdir  = "natSET-${charge}-${Vg}-${Vsd}";
+
+if(!defined($feconfig)){ $feconfig = "fe-config"; }
+do 'config.pm';
+do 'natdimensions.pm';
+do "${feconfig}.pm";
+
+
+system("./SET-input-allvars.pl $moleculename $charge $Vg $Vsd"
+       ." $dist_x $dist_y $oxideH"
+       ." $boxW $boxH $boxD"
+       ." $dielectric_constant"
+       ." $meshsuffix $outputdir  $feconfig");
