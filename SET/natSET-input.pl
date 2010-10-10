96ef983da3b302e3c7a913e731bb6269d0c0c6b0
James Avery <avery@diku.dk>
2010-10-10 21:51:57 +0200

    dist_y should *not* be read from dimensions.pm, since it is not read by SET-input-allvars.pl. Instead set dist_y += electrodeH in natSET-input.pl.

--- SET/natSET-input.pl
+++ SET/natSET-input.pl
@@ -12,6 +12,7 @@ do 'config.pm';
 do 'natdimensions.pm';
 do "${feconfig}.pm";
 
+$dist_y = $dist_y + $electrodeH;
 
 system("./SET-input-allvars.pl $moleculename $charge $Vg $Vsd"
        ." $dist_x $dist_y $oxideH"
