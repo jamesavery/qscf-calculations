5d22d36532c25481932e8befb434ca43905ee76f
James Avery <avery@diku.dk>
2010-10-10 21:33:32 +0200

    translate_y should be taken from dimensions.pm

--- SET/dimensions-allvars.pm
+++ SET/dimensions-allvars.pm
@@ -8,7 +8,7 @@ sub max { return $_[0]<=$_[1]? $_[1]: $_[0]; }
 
 %cm = $molecule{$moleculename}{'center of mass'};
 
-($dist_x,$dist_y,$oxideH,$boxW,$boxH,$boxD) = ($dist_x*$AA,$dist_y*$AA,$oxideH*$AA,$boxW*$AA,$boxH*$AA,$boxD*$AA);
+($dist_x,$dist_y,$oxideH,$boxW,$boxH,$boxD,$translate_y) = ($dist_x*$AA,$dist_y*$AA,$oxideH*$AA,$boxW*$AA,$boxH*$AA,$boxD*$AA,$translate_y*$AA);
 
 $vacuum_width    = $xmax-$xmin+2*$dist_x; # We want $dist_x distance from electrodes to nearest nucleus
 $vacuum_height   = $boxH-$oxideH;
@@ -16,7 +16,6 @@ $vacuum_height   = $boxH-$oxideH;
 $slice_depth     = $boxD;
 $electrode_width = ($boxW-$vacuum_width)/2;
 
-$translate_y  = $oxideH+$dist_y-$ymin;# We want $dist_y*Å distance from gate to nearest nucleus
 
 ($oxideW,$oxideH,$oxideD) = ($boxW,$oxideH,$boxD);
 
