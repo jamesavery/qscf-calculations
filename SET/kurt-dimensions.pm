155bbc9a0d39f5369d9ec0f5d9146e436d5905ae
James Avery <avery@diku.dk>
2011-03-06 21:33:11 +0100

    Dimensions were in Bohrs, and using outdated names.

--- SET/kurt-dimensions.pm
+++ SET/kurt-dimensions.pm
@@ -7,27 +7,28 @@ sub max { return $_[0]<=$_[1]? $_[1]: $_[0]; }
 
 %cm = $molecule{$moleculename}{'center of mass'};
 
-$vacuum_width    = 10.63*$AA;
-$vacuum_height   = 7.23*$AA;
-$slice_depth     = 12*$AA;
-$electrode_width = 4*$AA;
-$oxide_height    = 3.77*$AA;
-$gate_height     = 1.0*$AA;
-$dist_y          = 1.23*$AA;
-$translate_y     = 6*$AA;
+$vacuumW = 10.63;
+$vacuumH   = 7.23;
+$slice_depth     = 12;
+$electrodeW 	 = 4;
+$oxideH    = 3.77;
+$gateH     = 1.0;
+$dist_y          = 1.23;
+$translate_y     = 6;
 
 # Left and right electrode get respectively half the source-drain voltage Vsd: V_L = -V/2, V_R = V/2
 ($V_L,$V_R,$V_G) = (-0.5*$Vsd*$eV,0.5*$Vsd*$eV,$Vg*$eV); 
 
-($boxW,$boxH,$boxD) = ($vacuum_width+2*$electrode_width,
-		       $oxide_height+$vacuum_height,$slice_depth);
-($bW,$bH,$bD) = (18.63*$AA,11*$AA,12*$AA);
+($boxW,$boxH,$boxD) = ($vacuumW+2*$electrodeW,
+		       $oxideH+$vacuumH,$slice_depth);
+($bW,$bH,$bD) = (18.63,11,12);
 
 print STDERR "(boxW,boxH,boxD) = ($boxW,$boxH,$boxD) - should be ($bW,$bH,$bD)\n";
 
-($Oxide_W,$Oxide_H,$Oxide_D) = ($boxW,$oxide_height,$boxD);
+($oxideW,$oxideH,$oxideD) = ($boxW,$oxideH,$boxD);
+$electrodeH = $boxH-$vacuumH-$gateH;
 
 ($Hx,$Hy,$Hz) = ($boxW/2,$translate_y,$boxD/2);
 
-($tx,$ty,$tz) = (9.315*$AA,6*$AA,6*$AA);
+($tx,$ty,$tz) = (9.315,6,6);
 print STDERR "translate ($Hx,$Hy,$Hz) - should be ($tx,$ty,$tz)\n";
